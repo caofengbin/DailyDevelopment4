697ba74a2541668c78c630b8703daeeeb6ab9fce
caofengbin <1050430934@qq.com>
2017-07-19 20:43:09 +0800

    add code about draw

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice4DrawPointView.java
@@ -2,11 +2,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice4DrawPointView extends View {
+
+    private Paint paint = new Paint();
 
     public Practice4DrawPointView(Context context) {
         super(context);
@@ -27,5 +31,23 @@ public class Practice4DrawPointView extends View {
 //        练习内容：使用 canvas.drawPoint() 方法画点
 //        一个圆点，一个方点
 //        圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点
+
+        int totalWidth = getMeasuredWidth();
+        int totalHeight = getMeasuredHeight();
+
+        // 绘制圆点类型
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(80);
+        paint.setColor(Color.RED);
+        canvas.drawPoint(100, totalHeight / 2, paint);
+
+        //绘制方点类型
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawPoint(totalWidth - 100, totalHeight / 2, paint);
+
+        // 绘制BUTT类型
+        paint.setStrokeCap(Paint.Cap.BUTT);
+        canvas.drawPoint(totalWidth / 2, totalHeight / 2, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,17 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+
+    private Paint paint = new Paint();
+    private Path path = new Path();
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -24,6 +30,13 @@ public class Practice9DrawPathView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawPath() 方法画心形
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+
+        path.lineTo(100, 100);                  // 由当前位置 (0, 0) 向 (100, 100) 画一条直线
+        path.rLineTo(100, 0);                   // 由当前位置 (100, 100) 向正右方 100 像素的位置画一条直线
+
+        canvas.drawPath(path, paint);
     }
 }
